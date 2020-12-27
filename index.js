9a1b5cd2466c69062470a6d02e6848ddb18e6ca9
play.js <git@gituser.com>
2020-12-26 22:54:13 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const ms=require("./mystrings")
+
+let str ="this is a test string. i'll look for test substrings in this string"
+
+let srch = 
+{
+  locate:
+    [
+      [2,"test "],
+      [1," in"]
+    ],
+  replace:
+    [
+      [" ",""]
+    ]
+}
+
+console.log(srch.locate)
+let test=ms.FindBracketed(str,srch.locate,srch.replace);
+console.log(test)
