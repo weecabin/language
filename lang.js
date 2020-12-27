a8b890aaa94242a1c26cac0916dbe91f4fe3d5d6
play.js <git@gituser.com>
2020-12-27 09:37:33 -0800

    added search params

--- lang.js
+++ lang.js
@@ -1,8 +1,27 @@
-//const ms=require("./mystrings")
+const ms=require("./mystrings")
 const str="a misc string to be used as a search target. I'll create search locate parameters tp extract substrings out of this string";
+
+let srch = 
+{
+  locate:
+    [
+      [2,"a "],
+      [1," target"]
+    ],
+  replace:
+    [
+      [" ",""]
+    ]
+}
 
 function setup()
 {
   //window.alert("in setup");
   document.getElementById("srchstr").innerHTML=str;
+  document.getElementById("params").innerHTML=JSON.stringify(srch,null,2)
+}
+
+function extract()
+{
+  
 }
