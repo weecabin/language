f92e5430df1e3b79ec3e58e953c2174a28f26a70
play.js <git@gituser.com>
2020-12-27 15:42:12 -0800

    039

--- lang.js
+++ lang.js
@@ -1,13 +1,14 @@
 //const ms=require("./mystrings")
 let str="a misc string to be used as a search target. \n\
-Locate parameters (below) are used to extract substrings out of this string";
+Locate parameters (below) are used to extract substrings out of this string.\n\
+latlon 32.7336006165,-117.190002441 for KSAN";
 
 let srch = 
 {
   locate:
     [
-      [2,"a "],
-      [1," target"]
+      [1,"latlon"],
+      [1,"for"]
     ],
   replace:
     [
